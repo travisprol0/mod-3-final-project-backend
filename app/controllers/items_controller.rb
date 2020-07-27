2cada93e60e68b1560e47bf08e566d4dccc9c508
RitaHartnell <mel.soriano6123@gmail.com>
2020-07-27 18:00:50 -0400

    update item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,16 @@ class ItemsController < ApplicationController
         items = Item.all
         render json: items
     end
+
+    def update
+        item = Item.find[params[:id]]
+        item.update!(item_params)
+        render json: item
+    end
+
+    private
+
+    def item_params
+        params.require[:item].permit[:inventory_id]
+    end
 end
