132b4d385f7acd504751b544769246a59dc951f8
RitaHartnell <mel.soriano6123@gmail.com>
2020-07-28 16:42:34 -0400

    backgrounds 2: electroc boogaloo

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,9 +26,9 @@ recipe = Recipe.create(name: "Fishing Pole", advanced_item_id: advanced_item1.id
 rec2 = Recipe.create(name: 'Torch', advanced_item_id: two.id)
 rec3 = Recipe.create(name: 'Key', advanced_item_id: key.id)
 
-bedroom = Room.create(name: "Bedroom", img_url: 'https://cdn5.vectorstock.com/i/1000x1000/75/44/teenager-bedroom-cartoon-of-vector-20637544.jpg')
-living = Room.create(name: "Living Room", img_url: 'https://cdn1.vectorstock.com/i/1000x1000/84/40/cartoon-living-room-apartment-interior-vector-20598440.jpg')
-office = Room.create(name: "Office", img_url: 'https://cdn5.vectorstock.com/i/1000x1000/98/04/cartoon-home-office-interior-workplace-vector-20859804.jpg')
+bedroom = Room.create(name: "Bedroom", img_url: 'https://i.imgur.com/2148t5V.jpg')
+living = Room.create(name: "Living Room", img_url: 'https://i.imgur.com/sFJLjFi.jpg')
+office = Room.create(name: "Office", img_url: 'https://i.imgur.com/JzS9P36.jpg')
 
 item1 = Item.new(name: "String", img_url: 'https://rb.gy/raglyj', recipe_id: recipe.id, inventory_id: dumbinventory.id, room_id: bedroom.id)
 item1.save()
