8354853183a01f4dd16429047cbb87177a637722
RitaHartnell <mel.soriano6123@gmail.com>
2020-07-29 14:13:51 -0400

    inventory!

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,6 @@ class ItemsController < ApplicationController
     private
 
     def item_params
-        params.require(:item).permit(:inventory_id)
+        params.require(:item).permit(:inventory_id, :room_id)
     end
 end
