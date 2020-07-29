671be88223d05fd15830634e6d57a90909f9c93d
travisprol0 <travisjohnprol@gmail.com>
2020-07-29 15:33:37 -0400

    working popup menus

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,7 +1,6 @@
 class InventoriesController < ApplicationController
     def index
         inv = Inventory.first
-        # items = Inventory.items
         render json: inv, include: [:items]
     end
 end
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,5 @@
 class Inventory < ApplicationRecord
     belongs_to :character
     has_many :items
+    validates :items, length: {maximum: 5}
 end
