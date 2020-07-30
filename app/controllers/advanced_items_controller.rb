fb27457d212b9c507dcf935a3f14d74f93ca4dd0
travisprol0 <travisjohnprol@gmail.com>
2020-07-30 16:33:00 -0400

    error when adding advanced item

--- app/controllers/advanced_items_controller.rb
+++ app/controllers/advanced_items_controller.rb
@@ -3,4 +3,22 @@ class AdvancedItemsController < ApplicationController
         advItems = AdvancedItem.all
         render json: advItems
     end
+
+    def update
+        advItem = AdvancedItem.find(params[:id])
+        advItem.update!(advItem_params)
+        render json: advItem
+    end
+
+    def show
+        advItem = AdvancedItem.find(params[:id])
+        render json: advItem
+    end
+
+    private
+
+    def advItem_params
+        params.require(:advanced_item).permit(:inventory_id)
+    end
+
 end
