eed55b7fcd06cd5c05778029b12f15c4e6e8bbb5
travisprol0 <travisjohnprol@gmail.com>
2020-07-30 11:57:03 -0400

    first row

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
     def index
         recipes = Recipe.all
-        render json: recipes
+        render json: recipes, include: [:items, :advanced_item]
     end
 end
